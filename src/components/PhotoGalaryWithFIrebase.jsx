3c2d5ac9bfc0c44759817827a202c160294b33fd
George Cornea <george.cornea@outlook.com>
2023-05-04 18:56:05 -0700

    Added useReducer hook to manage the state Implemented state.photos and state.numberOfPictures

--- src/components/PhotoGalaryWithFIrebase.jsx
+++ src/components/PhotoGalaryWithFIrebase.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { CardList } from './CardList';
 import { Navbar } from './Navbar';
 import UploadForm from './UploadForm';
@@ -12,8 +12,33 @@ const data = [
   'https://picsum.photos/id/1006/200/200',
 ];
 
+const initialState = {
+  photos: data,
+  numberOfImages: data.length,
+  isCollapsed: false,
+  input: { title: null, file: null, path: null },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'addPhoto':
+      return {
+        ...state,
+        photos: [action.payload.path, ...state.photos],
+      };
+    case 'countImages':
+      return {
+        ...state,
+        numberOfImages: state.photos.length,
+      };
+    default:
+      return state;
+  }
+};
+
 export const PhotoGalaryWithFirebase = () => {
-  const [photos, setPhotos] = useState(data);
+  const [state, dispacher] = useReducer(reducer, initialState);
+  // const [photos, setPhotos] = useState(data);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [input, setInput] = useState({ title: null, file: null, path: null });
   const [numberOfImages, setNumberOfImages] = useState(0);
@@ -33,14 +58,22 @@ export const PhotoGalaryWithFirebase = () => {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    setPhotos([input.path, ...photos]);
+
+    // setPhotos([input.path, ...photos]);
+    // Updated with state from useReducer hook
+    dispacher({ type: 'addPhoto', payload: input });
+
     setInput({ title: null, file: null, path: null });
     setIsCollapsed(false);
   };
 
+  // useEffect(() => {
+  //   setNumberOfImages(photos.length);
+  // }, [photos]);
+  // updated to use state from useReducer hook
   useEffect(() => {
-    setNumberOfImages(photos.length);
-  }, [photos]);
+    dispacher({ type: 'countImages' });
+  }, [state.photos]);
 
   return (
     <>
@@ -63,10 +96,12 @@ export const PhotoGalaryWithFirebase = () => {
         )}
         <h1>Photo Gallery</h1>
         <p>
-          You have {numberOfImages} image{numberOfImages === 1 ? '' : 's'} in
-          your gallery
+          You have {state.numberOfImages} image
+          {state.numberOfImages === 1 ? '' : 's'} in your gallery
         </p>
-        <CardList photos={photos} />
+        {/* <CardList photos={photos} /> */}
+        {/* Updated with state from useReducer hook */}
+        <CardList photos={state.photos} />
       </div>
     </>
   );
